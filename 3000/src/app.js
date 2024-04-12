2a352cadc691e69dac2a60fef95d0e29c91b0567
jovid_18 <cshcho99@gmail.com>
2024-04-12 21:56:07 +0900

    FEAT: init project

---
+++ 3000/src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import notFoundErrorHandler from './middlewares/notFoundError.middleware.js';
+import generalErrorHandler from './middlewares/generalError.middleware.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>3000번</h1>');
+});
+
+app.get('/3000/:number', (req, res) => {
+  const number = req.params.number;
+
+  res.send(`<h1>3000번 ${number} 주소</h1>`);
+});
+
+app.use(notFoundErrorHandler);
+app.use(generalErrorHandler);
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
